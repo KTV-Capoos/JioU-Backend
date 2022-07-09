a79e67d464d5aa58454e05a886673cab81ee2d1b
Wen Junhua <wenjh1998@gmail.com>
2022-07-09 16:14:22 +0800

    feat: add event id test

--- admin/events/tests.py
+++ admin/events/tests.py
@@ -3,9 +3,15 @@ from django.test import TestCase, Client
 # Create your tests here.
 class EventTest(TestCase):
     def setUp(self):
+        """Set up the test client"""
         self.client = Client()
 
     def test_all_events(self):
         """Test retrieve all events"""
         response = self.client.get('/events/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_event_id(self):
+        """Test event details"""
+        response = self.client.get('/events/1')
         self.assertEqual(response.status_code, 200)
