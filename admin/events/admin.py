a80615dfe65c293bb10095573ebd4d7fd3a9a236
Wen Junhua <wenjh1998@gmail.com>
2022-07-09 16:22:27 +0800

    style: update formatting

--- admin/events/admin.py
+++ admin/events/admin.py
@@ -7,7 +7,13 @@ from .models import Event
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy: Optional[str] = 'event_date'
-    list_display = ('event_name', 'event_date', 'event_time', 'event_duration', 'event_location')
+    list_display = (
+        'event_name',
+        'event_date',
+        'event_time',
+        'event_duration',
+        'event_location'
+    )
 
 
 admin.site.register(Event, EventAdmin)
